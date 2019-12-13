7dbc059f735ea5637348b25412f00ba0ecfcd07a
Alien <Ztiany@users.noreply.github.com>
2019-12-13 17:22:04 +0800

    Update LoadMoreView.java

--- library/src/main/java/com/ztiany/loadmore/adapter/LoadMoreView.java
+++ library/src/main/java/com/ztiany/loadmore/adapter/LoadMoreView.java
@@ -8,11 +8,7 @@ import android.widget.TextView;
 
 
 /**
- * author Ztiany                                                                        <br/>
- * email 1169654504@qq.com & ztiany3@gmail.com           <br/>
- * date 2016-04-11 13:18                                                       <br/>
- * description                                                                             <br/>
- * version
+ * @author Ztiany
  */
 public class LoadMoreView extends FrameLayout implements ILoadMoreView {
 
